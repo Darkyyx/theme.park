03ccfa82c77912067e90cca68b19e00c2aae8fc2
Darkyyx <deityxi@qq.com>
2025-01-06 06:51:45 +0800

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,11 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre> ____     ___    ___   _   _     ____    ____ 
-|  _ \   / _ \  |_ _| | \ | |   / ___|  / ___|
-| |_) | | | | |  | |  |  \| |  | |     | |    
-|  _ <  | |_| |  | |  | |\  |  | |___  | |___ 
-|_| \_\  \___/  |___| |_| \_| (_)____|  \____|
-                                              </pre> </div>
+<div class="custom-text-header"> <pre> _          _                ____       _                        
+| |    __ _| | _____        |  _ \ ___ (_)_ __           ___ ___ 
+| |   / _` | |/ / _ \       | |_) / _ \| | '_ \         / __/ __|
+| |__| (_| |   <  __/   _   |  _ < (_) | | | | |   _   | (_| (__ 
+|_____\__,_|_|\_\___|  (_)  |_| \_\___/|_|_| |_|  (_)   \___\___|
+                                                                 </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
